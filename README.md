3b0d1747b3b3b1dbdf7c69c7680d6d88dd347cf4
Ivan Stoimenov <ivanstoimenovdb@gmail.com>
2025-10-23 18:55:55 +0300

    Show homepage.

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Excercise 1 Soft Uni
     -[X] Debugging;
 - [X] Create basic node server.
 - [X] Add resources.
+- [X] Open homepage.
+- [X] Add css
+- [X] Extract css into separate js files
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import http from 'http';
+import home from './home.html.js';
+import style from './style.css.js';
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World');
+    if (req.url === '/') {
+        res.writeHead(200, {
+            "content-type": 'text/html',
+        })
+
+        res.write(home);
+    } else if (req.url === '/styles/site.css') {
+        res.writeHead(200, {
+            "content-type": "text/css",
+        })
+
+        res.write(style);
+    }
 
     res.end();
 })
